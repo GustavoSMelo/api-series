2f8dc9cb0f892c05164abcdbc38b80797a84797f
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-07-28 20:24:34 -0300

    refactoring and add method index and show

--- app/Http/Controllers/SeriesControllers.php
+++ app/Http/Controllers/SeriesControllers.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Http\Request;
 use App\Models\Series;
+use DomainException;
 
 class SeriesControllers extends Controller {
     public function store (Request $request) {
@@ -14,21 +15,52 @@ class SeriesControllers extends Controller {
 
             $serieModel = new Series();
 
-            $serieModel->name = $request->input('name');
-            $serieModel->sinopse = $request->input('sinopse');
-            $serieModel->genre = $request->input('genre');
-            $serieModel->age_recommended = $request->input('age_recommended');
-
+            $serieModel->name = $request->name;
+            $serieModel->sinopse = $request->sinopse;
+            $serieModel->genre = $request->genre;
+            $serieModel->age_recommended = $request->age_recommended;
             $serieModel->save();
 
             return response()->json([
                 'message' => 'serie created with success '
-            ]);
+            ], 201);
 
         } catch (Exception $err) {
             return response()->json([
                 'error' => 'error to create a new serie '. PHP_EOL . $err
             ], 401);
+        }
+    }
+
+    public function index () {
+        try {
+
+            $serieModel = new Series();
+
+            return $serieModel->all();
+
+        } catch (Exception $err) {
+            return response()->json([
+                'Error' => 'error to get all series'
+            ], 500);
+        }
+    }
+
+    public function show (int $id) {
+        try {
+            $serieModel = new Series();
+
+            $serie = $serieModel->find($id);
+
+            if (empty($serie)) {
+                throw new DomainException('serie not found');
+            }
+
+            return $serie;
+        } catch (DomainException $err) {
+            return response()->json([
+                'Error' => $err->getMessage()
+            ], 404);
         }
     }
 }
