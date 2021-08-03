2cb0c438ecc7a5bff9eb0ebaaeb906f464f201ec
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-08-02 21:09:41 -0300

    New routes in project

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,15 @@ $router->group(['prefix' => 'api'], function () use ($router) {
         return $router->app->version();
     });
 
-    $router->get('/series', 'SeriesControllers@index');
-    $router->get('/series/{id}', 'SeriesControllers@show');
-    $router->post('/series', 'SeriesControllers@store');
-    $router->put('/series/{id}', 'SeriesControllers@update');
-    $router->delete('/series/{id}', 'SeriesControllers@destroy');
+    $router->group(['prefix' => 'series'], function () use ($router) {
+        $router->get('', 'SeriesControllers@index');
+        $router->post('', 'SeriesControllers@store');
+        $router->get('{id}', 'SeriesControllers@show');
+        $router->put('{id}', 'SeriesControllers@update');
+        $router->delete('{id}', 'SeriesControllers@destroy');
+    });
+
+    $router->group(['prefix' => 'episodeos'], function () use ($router) {
+        $router->get('', 'EpisodeosController@index');
+    });
 });
