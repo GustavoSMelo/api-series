05cc533779142f380e6fab526a4be4e754727cf6
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-08-03 20:43:21 -0300

    add number of paginations

--- app/Models/Episodes.php
+++ app/Models/Episodes.php
@@ -5,10 +5,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Episodes extends Model {
 
-    protected $fillable = ['name', 'season', 'ep_number', 'watched'];
+    protected $fillable = ['name', 'season', 'ep_number', 'watched', 'serie_id'];
+    protected $perPage = 5;
 
     public function series()
     {
         $this->belongsTo(Series::class);
+    }
+
+    public function getWatchedAttribute ($value) : bool {
+        return (bool) $value;
     }
 }
