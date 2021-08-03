8e39c45f0e532d718ef1fbab90a34f13627b968e
Gustavo Santos Melo <gsantos15569@gmail.com>
2021-08-03 20:42:38 -0300

    create function perSeries with pagination

--- app/Http/Controllers/EpisodeosController.php
+++ app/Http/Controllers/EpisodeosController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Models\Episodes;
+use DomainException;
+use Exception;
+use Illuminate\Http\Request;
 
 class EpisodeosController extends BaseCustomController {
     public function __construct() {
         parent::__construct(Episodes::class);
+    }
+
+    public function perSeries (int $id, Request $request) {
+        try {
+            $offset = ($request->page - 1) * 5;
+            $episodes = Episodes::query()
+                ->where('serie_id', $id)
+                ->offset($offset)
+                ->limit(5)
+                ->get();
+
+            if(count($episodes) <= 0) {
+                throw new DomainException();
+            }
+
+            return $episodes;
+
+        } catch (Exception $err) {
+            return response()->json([
+                'Error' => 'Could not find any episodeos in this serie'
+            ], 404);
+        }
     }
 }
